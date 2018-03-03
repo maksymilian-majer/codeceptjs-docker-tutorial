171dc28222a87e5833f4f9e7354b27828862d287
Maksymilian Majer <maks@itcraftship.com>
2018-03-04 00:10:47 +0100

    Added more tests for Login feature

--- tests/01_security/01_login_test.js
+++ tests/01_security/01_login_test.js
@@ -7,3 +7,38 @@ Scenario('Login page has Username and Password labels', (I, Security) => {
     I.see('Username or Email Address');
     I.see('Password');
 });
+
+
+Scenario('Password field is required', (I, Security) => {
+    I.amOnPage('/wp-login.php');
+    I.fillField('Username or Email Address', 'maks');
+    I.click('Log In');
+    I.see('ERROR: The password field is empty.');
+});
+
+
+Scenario('Password field is required', (I, Security) => {
+    I.amOnPage('/wp-login.php');
+    I.fillField('Username or Email Address', 'maks');
+    I.fillField('Password', 'secureP@ssw0rd1');
+    I.click('Log In');
+    I.see('ERROR: Invalid username. Lost your password?');
+});
+
+
+Scenario('Password field is required', (I, Security) => {
+    I.amOnPage('/wp-login.php');
+    I.fillField('Username or Email Address', 'maks');
+    I.fillField('Password', 'secureP@ssw0rd1');
+    I.click('Log In');
+    I.see('ERROR: Invalid username. Lost your password?');
+});
+
+Scenario('Login with correct credentials', (I, Security) => {
+    I.amOnPage('/wp-login.php');
+    I.fillField('Username or Email Address', 'admin');
+    I.fillField('Password', 'secureP@ssw0rd1');
+    I.click('Log In');
+    I.seeInCurrentUrl('/wp-admin/');
+    I.see('Welcome to WordPress!');
+});
