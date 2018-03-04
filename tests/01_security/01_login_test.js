88560647140d43ac18dc05bc5add401c2fc501fd
Maksymilian Majer <maks@itcraftship.com>
2018-03-04 09:18:13 +0100

    Extracted common login steps

--- tests/01_security/01_login_test.js
+++ tests/01_security/01_login_test.js
@@ -2,14 +2,14 @@
 
 Feature('Login @current');
 
-Scenario('Login page has Username and Password labels', (I, Security) => {
+Scenario('Login page has Username and Password labels', (I) => {
     I.amOnPage('/wp-login.php');
     I.see('Username or Email Address');
     I.see('Password');
 });
 
 
-Scenario('Password field is required', (I, Security) => {
+Scenario('Password field is required', (I) => {
     I.amOnPage('/wp-login.php');
     I.fillField('Username or Email Address', 'maks');
     I.click('Log In');
@@ -19,18 +19,14 @@ Scenario('Password field is required', (I, Security) => {
 
 Scenario('Invalid username or password', (I, Security) => {
     I.amOnPage('/wp-login.php');
-    I.fillField('Username or Email Address', 'maks');
-    I.fillField('Password', 'some wrong psw');
-    I.click('Log In');
+    Security.loginWith('maks', 'some wrong password');
     I.see('ERROR: Invalid username. Lost your password?');
 });
 
 
 Scenario('Login with correct credentials', (I, Security) => {
     I.amOnPage('/wp-login.php');
-    I.fillField('Username or Email Address', 'admin');
-    I.fillField('Password', 'secureP@ssw0rd1');
-    I.click('Log In');
+    Security.loginAsAdmin();
     I.seeInCurrentUrl('/wp-admin/');
     I.see('Welcome to WordPress!');
 });
--- tests/01_security/steps.js
+++ tests/01_security/steps.js
@@ -1,14 +1,35 @@
-/// <reference path="../../steps.d.ts" />
 module.exports = {
 
   _init() {
     I = actor();
   },
 
-  loginWith: function (username, password) {
+  loginUrl: '/wp-login.php',
+
+  // in a real world scenario you'll want to get those
+  // from environment variables:
+  adminCredentials: {
+    user: 'admin',
+    password: 'secureP@ssw0rd1'
   },
 
-  loginAsAdmin: function () {
-    this.loginWith('todo', 'todo');
+  // setting locators
+  fields: {
+    user: 'Username or Email Address',
+    password: 'Password'
+  },
+  loginButton: 'Log In',
+
+  // introducing methods
+
+  loginWith(username, password) {
+    I.amOnPage(this.loginUrl);
+    I.fillField(this.fields.user, username);
+    I.fillField(this.fields.password, password);
+    I.click(this.loginButton);
+  },
+
+  loginAsAdmin() {
+    this.loginWith(this.adminCredentials.user, this.adminCredentials.password);
   },
 };
